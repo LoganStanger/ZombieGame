2fe4d3fcd7cc8ce0f1783c6039e64dbfcce1e014
LoganStanger <24296@burnside.school.nz>
2026-05-28 14:39:54 +1200

    Third commit

--- sciptQuestionMark/player.gd
+++ sciptQuestionMark/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-var directionDash = Vector2()
 var IsDashing: bool = false
 var IsSprinting: bool = false
+var TrueDashing: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,18 +11,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ZombieBite"):
+		
+		await get_tree().create_timer(0.1).timeout
 		IsDashing = true
-		Booyah.walk_speed += 10000
-		Booyah.run_speed += 10000
-		await get_tree().create_timer(0.5).timeout
-		Booyah.walk_speed -= 10000
-		Booyah.run_speed -= 10000
+		Booyah.walk_speed += 500000
+		Booyah.run_speed += 500000
+		await get_tree().create_timer(0.1).timeout
+		Booyah.walk_speed -= 500000
+		Booyah.run_speed -= 500000
 		IsDashing = false
-	
-	if Input.is_action_pressed("ZombieSprint"):
+		
+		
+	if Input.is_action_just_pressed("ZombieSprint") and IsSprinting == false:
 		IsSprinting = true
-	else:
+		Booyah.hungerSpeed += 1
+	if Input.is_action_just_released("ZombieSprint") and IsSprinting == true:
 		IsSprinting = false
+		Booyah.hungerSpeed -= 1
 		
 	if IsSprinting == false:
 		var dir = Input.get_vector("ZombieLeft", "ZombieRight", "ZombieUp", "ZombieDown")
