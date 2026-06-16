644a22ff0a3651f09229fb38634cf78b991f58ca
LoganStanger <24296@burnside.school.nz>
2026-06-17 08:51:57 +1200

    Thirteenth commit (YIPEEEEE)

---
+++ sciptQuestionMark/body_6.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Booyah.level == 2:
+		queue_free()
+	if Booyah.level == 3:
+		queue_free()
+	if Booyah.level == 6:
+		queue_free()
+	if Booyah.level == 8:
+		queue_free()
+	if Booyah.level == 10:
+		queue_free()
--- sciptQuestionMark/elevator.gd
+++ sciptQuestionMark/elevator.gd
@@ -12,7 +12,6 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("loganArea") and Booyah.keycardyep:
-		Booyah.level += 1
 		if Booyah.TrueDashing == true and Booyah.IsDashing == false:
 			Booyah.TrueDashing = false
 			Booyah.walk_speed += 5000
@@ -25,4 +24,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		if Booyah.level == 10:
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/Kentucky.tscn")
 		else:
+			Booyah.level += 1
 			get_tree().call_deferred("change_scene_to_file", "res://Scenes/regent.tscn")
